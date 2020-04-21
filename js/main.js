1949c8c6a38a3b08f4a2c42597fa8db6cfb1872d
been_yan <asdfghjkl42045@gmail.ocm>
2020-04-21 22:27:59 +0800

    none

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+    $(".guide,#dark_scrim").click(() => { //左側遮布
+        if ($("tool-show").attr("opened") === undefined){
+            $("tool-show,#dark_scrim").attr("opened","");
+            $("body").css("overflow","hidden");
+        }
+        else {
+            $("tool-show,#dark_scrim").removeAttr("opened");
+            $("body").css("overflow","auto");
+        }
+    });
+})
