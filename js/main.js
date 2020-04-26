d88195529da5ca007cdad2a5423495a6b35a5ab5
been_yan <asdfghjkl42045@gmail.ocm>
2020-04-26 21:17:25 +0800

    null

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
 $(function(){
+    $(".content:not(:eq(0))").prepend(`<div class="package" style="width: 100%;"><div class="bar" check="true"><span>套用</span><div class="bar_check"><div class="bar_background"><div class="bar_round"></div></div></div></div></div>`);
+    ["px","em","%","vw","vh","vmin","vmax","in","cm","mm","rem"].forEach(e=>{$(".unit").append(`<option value="${e}">${e}</option>`)});
+    $("input[type=number]").keyup(e => {
+        let self = e.currentTarget;
+        $(self).val(($(self).val() > 1000)?1000:$(self).val());
+    });
     $(".guide,#dark_scrim").click(() => { //左側遮布
-        if ($("tool-show").attr("opened") === undefined){
-            $("tool-show,#dark_scrim").attr("opened","");
+        if ($("tool-bar").attr("opened") === undefined){
+            $("tool-bar,#dark_scrim").attr("opened","");
             $("body").css("overflow","hidden");
         }
         else {
-            $("tool-show,#dark_scrim").removeAttr("opened");
+            $("tool-bar,#dark_scrim").removeAttr("opened");
             $("body").css("overflow","auto");
         }
     });
+    $(".fake_checkbox,.bar").click(e => {
+        let self = e.currentTarget;
+        $(self).attr("check",$(self).attr("check") === "false");
+    });
 })
-try {
-    
-} catch (error) {
-    
-}
