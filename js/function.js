9e8695168a059551addae71f7105fe9d981c18be
been_yan <asdfghjkl42045@gmail.ocm>
2020-05-03 21:58:51 +0800

    do

--- js/function.js
+++ js/function.js
@@ -3,3 +3,34 @@ function getval(select){
     $(select).each((index,self) => {temp.push($(self).val())});
     return temp;
 };
+function filter_ck(select){
+    let temp = [];
+    $(select).each((index,e) => {if ($(e).attr("check")==="false") temp.push(`#${$(e).attr("for")} *`)});
+    return temp;
+}
+function scrollbar_button(self){
+    $("#scrollbar_button .fake_checkbox").each((index,e) => {
+        let alltext = "";
+        if ($(e).attr("check") === "true"){
+            // let filter = (($(`#${id} .bar:eq(0)`).attr("check")==="false")?".active *":"null")+(($(`#${id} .bar:eq(1)`).attr("check")==="false")?",.hover *":"null");
+            let name = ["main-img","main-color","main-border","active-img","active-color","active-border","hover-img","hover-color","hover-border"];
+            let id = $(e).attr("for");
+            main_val = {
+                "main" : getval(`#${id} *:not(.active *,.hover *):not(.img-show *,.color-show *,.border-show *)>input,#${id} *:not(.active *,.hover *):not(.img-show *,.color-show *,.border-show *)>select`),
+                "main-img" : getval(`#${id} .img-show:eq(0) input,#${id} .img-show:eq(0) select`),
+                "main-color" : getval(`#${id} .color-show:eq(0) input:not([type=color])`),
+                "main-border" : getval(`#${id} .border-show:eq(0) input:not([type=color]),#${id} .border-show:eq(0) select`),
+                "active-img" : getval(`#${id} .img-show:eq(1) input,#${id} .img-show:eq(1) select`),
+                "active-color" : getval(`#${id} .color-show:eq(1) input:not([type=color])`),
+                "active-border" : getval(`#${id} .border-show:eq(1) input:not([type=color]),#${id} .border-show:eq(1) select`),
+                "hover-img" : getval(`#${id} .img-show:eq(2) input,#${id} .img-show:eq(2) select`),
+                "hover-color" : getval(`#${id} .color-show:eq(2) input:not([type=color])`),
+                "hover-border" : getval(`#${id} .border-show:eq(2) input:not([type=color]),#${id} .border-show:eq(2) select`),
+            }
+            $(`#${id} .fake_checkbox`).each((index,e) => {
+                if ($(e).attr("check") === "false") main_val[name[index]] = main_val[name[index]].map(e => {return "unset"});
+            });
+            console.log(main_val);
+        }
+    });
+}
--- js/main.js
+++ js/main.js
@@ -35,28 +35,39 @@ $(function(){
         let val = getval(".content:eq(1) input[type=number],.content:eq(1) .unit");
         $(".webkitscrollbar_main").text(`*::-webkit-scrollbar{width:${val[0]+val[1]};height:${val[2]+val[3]}}`);
     });
-    $(".theme").click(e => {
+    $(".theme").click(e => { //套用樣式
         let self = e.currentTarget;
-        let css_array = [".webkitscrollbar"];
+        let css_array = [".webkitscrollbar","webkitscrollbar_button"];
         let index = $(".theme").index(self);
         if ($(self).attr("check") === "true") $(css_array[index]).attr("type","text/css");
         else $(css_array[index]).attr("type","unset");
     });
-    $("body").on("change","[type=color]",e => {
+    $("body").on("change","[type=color]",e => { //顏色改變
         let self = e.currentTarget;
         $(self).siblings(".keyin").val($(self).val());
         $(self).siblings(".keyin").css("color",$(self).val());
     })
-    $("body").on("click",".increase",e => {
+    $("body").on("click",".increase",e => { //增加顏色
         let self = e.currentTarget;
         let num = parseInt($(self).parents(".color-show").children(".color-main").last().children().children("span").text());
         $(self).parents(".color-show").children(".margin").before(`<div class="subb package color-main" style="display: block;"><div class="color"><span>${num + 1}.</span><div class="color-picker"><input type="color" value="#E4E6EB"><input type="text" class="keyin" value="#E4E6EB"></div><div class="delete-color">-</div></div></div>`);
     });
-    $("body").on("click",".delete-color",e => {
+    $("body").on("click",".delete-color",e => { //減少顏色
         let self = e.currentTarget;
         let colormain = $(self).parents(".color-show");
         $(self).parents(".color-main").remove(); //刪除自己
         colormain.children(".color-main").each((index,e) => {$(e).children().children("span").text(index+1+".")}); //重整數字
+    });
+    $("body").on("change",".button-show input",e => {
+        let self = e.currentTarget;
+        scrollbar_button(self);
+    });
+    $("body").on("click","#scrollbar_button .fake_checkbox,.button-show .bar,.button-show .fake_checkbox",e => {
+        let self = e.currentTarget;
+        scrollbar_button(self);
+    });
+    $("body").on("","",e => {
+        let self = e.currentTarget;
     });
     $("body").on("","",e => {
         let self = e.currentTarget;
